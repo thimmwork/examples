5fa593bec5ee4d9e52852aec7cebe9eef4d82e7b
marcel <git@thimmwork.net>
2018-12-02 08:24:39 +0100

    initial checkin of spring boot 1.5 example

---
+++ spring15/src/test/java/net/thimmwork/testing/junit4/testcontainers/springboot15/FlexiScopeTestcontainerSuiteTest.java
@@ -0,0 +1,85 @@
+package net.thimmwork.testing.junit4.testcontainers.springboot15;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import javax.persistence.Entity;
+import java.util.Properties;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({FlexiScopeTestcontainerSuiteTest.FlexiScopeTestClass.class, FlexiScopeTestcontainerSuiteTest.FlexiScopeTestClass.class})
+public class FlexiScopeTestcontainerSuiteTest {
+
+    @ClassRule public static PostgreSQLContainer POSTGRES = new PostgreSQLContainer();
+
+    @ClassRule
+    public static KafkaContainer KAFKA = new KafkaContainer().withEmbeddedZookeeper();
+
+    @TestConfiguration
+    public static class TestConfig {
+        @Bean
+        public PropertySourcesPlaceholderConfigurer config() {
+            final PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+            Properties properties = new Properties();
+
+            properties.setProperty("spring.datasource.url", POSTGRES.getJdbcUrl());
+            properties.setProperty("spring.datasource.username", POSTGRES.getUsername());
+            properties.setProperty("spring.datasource.password", POSTGRES.getPassword());
+
+            properties.setProperty("spring.kafka.bootstrap-servers", KAFKA.getBootstrapServers().replace("PLAINTEXT://", ""));
+
+            pspc.setProperties(properties);
+            pspc.setLocalOverride(true); //this will give our properties higher priority than application.yml
+            return pspc;
+        }
+    }
+
+    @SpringBootTest
+    @RunWith(SpringRunner.class)
+    public static class FlexiScopeTestClass {
+
+        //note that each test class in the suite uses the same instance of the semaphore instantiated by the suite class
+        //this way, beforeSuite() and beforeClass() are invoked on the same instance.
+        @ClassRule @Rule
+        public static PostgreSQLContainer DATABASE = FlexiScopeTestcontainerSuiteTest.POSTGRES;
+
+        @Autowired
+        private Repo repo;
+
+        @Test
+        public void setUp_is_called_only_once_in_test_suite() {
+            TestEntity testEntity = new TestEntity();
+            testEntity.id = UUID.randomUUID();
+
+            repo.save(testEntity);
+            TestEntity persisted = repo.findOne(testEntity.id);
+
+            assertEquals(testEntity.id, persisted.id);
+        }
+    }
+
+    @Entity
+    public static class TestEntity {
+        private UUID id;
+    }
+
+    @Repository
+    public interface Repo extends CrudRepository<TestEntity, UUID> {}
+
+}
